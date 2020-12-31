b613cc9088c4deecd8dbfc403d06ee3ab723becf
korea <korea@DESKTOP-QFQHHIN>
2020-12-31 19:13:16 +0900

    3.0

--- src/main/java/com/example/demo/conf/SwaggerConfig.java
+++ src/main/java/com/example/demo/conf/SwaggerConfig.java
@@ -2,6 +2,10 @@ package com.example.demo.conf;
 
 import static springfox.documentation.builders.PathSelectors.regex;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,9 +21,14 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
 	@Bean
     public Docket exampleApi() {
+		Set<String> protocols = new HashSet<>();
+		protocols.add("http");
+		protocols.add("https");
         return new Docket(DocumentationType.SWAGGER_2)
+        		.protocols(protocols)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.example.demo.api"))
+                
                 .paths(regex("/api.*"))
                 .build()
                 .apiInfo(metaInfo());
@@ -34,7 +43,8 @@ public class SwaggerConfig {
                 new Contact("yourname", "https://github.com/yourlink",
                         "youremail@yourdomain.com"),
                 "Apache License Version 2.0",
-                "https://www.apache.org/licesen.html"
+                "https://www.apache.org/licesen.html",
+                new ArrayList<>()
         );
 
         return apiInfo;
