aed2486e4468cafbf3213a5558ffac49d8dc6456
korea <korea@DESKTOP-QFQHHIN>
2021-01-01 14:56:48 +0900

    swag

--- src/main/java/com/example/demo/conf/SwaggerConfig.java
+++ src/main/java/com/example/demo/conf/SwaggerConfig.java
@@ -7,46 +7,31 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableSwagger2
-@Configuration
-public class SwaggerConfig {
-	@Bean
-    public Docket exampleApi() {
-		Set<String> protocols = new HashSet<>();
-		protocols.add("http");
-		protocols.add("https");
-        return new Docket(DocumentationType.SWAGGER_2)
-        		.protocols(protocols)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.demo.api"))
-                
-                .paths(regex("/api.*"))
-                .build()
-                .apiInfo(metaInfo());
-    }
-	private ApiInfo metaInfo() {
-
-        ApiInfo apiInfo = new ApiInfo(
-                "Spring Boot Swagger Example API",
-                "Spring Boot Swagger Example Api Creator",
-                "1.0",
-                "Terms of Service",
-                new Contact("yourname", "https://github.com/yourlink",
-                        "youremail@yourdomain.com"),
-                "Apache License Version 2.0",
-                "https://www.apache.org/licesen.html",
-                new ArrayList<>()
-        );
-
-        return apiInfo;
-    }
+public class SwaggerConfig implements WebMvcConfigurer{
+	
+	final String baseUrl = StringUtils.trimTrailingCharacter("http://210.117.172.165:8085", '/');
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		
+		registry.addResourceHandler(baseUrl + "/swagger-ui/**")
+			.addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/")
+			.resourceChain(false);
+	}
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController(baseUrl + "/swagger-ui/")
+			.setViewName("forward:"+baseUrl+"/swagger-ui/index.html");
+	}
 }
